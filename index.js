49a9cc5fa9205ec633663a5faeef1710ef262016
NiclanX <nicardolannaman@gmail.com>
2025-06-13 23:11:55 -0500

    New CSS, JS & HTML

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 
 const clicker = document.getElementById('clicker');
 // const displayer = document.querySelector('.crm-form');
-const formCtrl = document.querySelector('.form-ctrl');
+const closeBtn = document.querySelector('.close-btn');
+const formCtrl = document.querySelector('.form-bg');
+const overlay = document.querySelector('.overlay');
 
 
 clicker.addEventListener('click', () => {
     
     // displayer.classList.toggle('show');
     formCtrl.classList.toggle('show');
+    overlay.classList.add('show');
 
 });
+
+closeBtn.addEventListener('click', ()=>{
+    if (formCtrl.classList.contains('show')) {
+        formCtrl.classList.toggle('show');
+        overlay.classList.toggle('show');
+    }
+})
 
